aed3f5ad8fc20eb092012b31a6504e14b3f916f8
Виталий Ярошенко <yarokha@gmail.com>
2017-03-26 17:05:05 +0300

    ;..

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
+#include <vector>
 void main() {
-	double a = 7.5, b = 2.1 , d = a/b;
-	std::cout << d << std::endl << (int)d;
+	int t = 0;
+	int b[4] = { 1,2,3,4 };
+	std::vector< (int)4 > a;
+	for (int i = 0; i < 3; i++)
+			a.push_back(b);
+	
 	system("pause");
 }
--- SAScore.cpp
+++ SAScore.cpp
@@ -1,5 +1,6 @@
 #include "SAScore.h"
-
+#include <iostream>
+#include <cstdio>
 
 
 SAScore::SAScore(double **data, double r, double step)
