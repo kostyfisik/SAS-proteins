eaadbb5a67226f9e278067b6ab10f1f043bd9660
Виталий Ярошенко <yarokha@gmail.com>
2017-05-04 02:25:31 +0300

    full update

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 #include "SAScore.h"
 void main() {
-	std::vector < std::vector<double> > m(1, { 1,2,3, 50 });
-	//m.push_back({ 8,2,3,3 });
+	std::vector < std::vector<double> > m(1,{ 1,2,3, 9 });
+	m.push_back({ 8,2,3,3 });
 	SAScore <> b (m, 1, 0.5);
 	system("pause");
 }
